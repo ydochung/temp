7ca82af29400eb7f7fa1dd2aa305d1d6f5d666aa
ydochung <ydochung@stanford.kr>
2017-11-05 11:51:26 -0800

    Chapter2 code

--- src/AcceleratedCpp.cpp
+++ src/AcceleratedCpp.cpp
@@ -19,30 +19,53 @@ int main() {
 	   *              *
 	   ****************
 	 * */
+
+	// ask for the person's name
 	cout << "Please enter your first name: ";
+
+	// read the name
 	string name;
 	cin >> name;
 
 	// build the message that we intend to write
 	const string greeting = "Hello, " + name + "!";
 
-	// build the second and forth lines of the output
-	const string spaces(greeting.size(), ' ');
-	const string second = "* " + spaces + " *";
+	// the number of blanks surrounding the greeting
+	const int pad = 1;
 
-	// build the first and fifth lines of the output
-	const string first(second.size(), '*');
+	// the number of rows and columns to write
+	const int rows = pad*2 +3;
+	const string::size_type cols = greeting.size() + pad *2 + 2;
 
-	// write it all
+	// write a blank line to separate the output from the input
 	cout << endl;
-	cout << first << endl;
-	cout << second << endl;
-	cout << "* " << greeting << " *" << endl;
-	cout << second << endl;
-	cout << first << endl;
 
-	const string hello = "hello";
-	const string message = hello + ", world" + "!";
+	// write |rows| rows of output
+	// invariant: we have written r rows so far
+	for (int r = 0; r != rows; ++r){
+		string::size_type c= 0;
+
+		//invariant: we have written c characters so far in the current row
+		while (c != cols){
+			// is it time to write the greeting?
+			if (r == pad + 1 && c == pad + 1){
+				cout << greeting;
+				c += greeting.size();
+			}
+			else{
+				// are we on the border?
+				if (r == 0 || r == rows -1 || c == 0 || c == cols-1){
+					cout << "*";
+				}
+				else{
+					cout << " ";
+				}
+				++c;
+			}
+		}
+		cout << endl;
+	}
+
 
 	return 0;
 }
