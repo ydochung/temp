4178e7827f2eb164bc6306b2d10e151659fe6719
ydochung <ydochung@stanford.kr>
2017-11-05 11:40:29 -0800

    Chapter1 code

--- src/AcceleratedCpp.cpp
+++ src/AcceleratedCpp.cpp
@@ -10,6 +10,39 @@
 using namespace std;
 
 int main() {
-	cout << "!!!Hello World!!!" << endl; // prints !!!Hello World!!!
+
+	/*
+	 * Please enter your first name: Alex
+	 * ****************
+	   *              *
+	   * Hello, Alex! *
+	   *              *
+	   ****************
+	 * */
+	cout << "Please enter your first name: ";
+	string name;
+	cin >> name;
+
+	// build the message that we intend to write
+	const string greeting = "Hello, " + name + "!";
+
+	// build the second and forth lines of the output
+	const string spaces(greeting.size(), ' ');
+	const string second = "* " + spaces + " *";
+
+	// build the first and fifth lines of the output
+	const string first(second.size(), '*');
+
+	// write it all
+	cout << endl;
+	cout << first << endl;
+	cout << second << endl;
+	cout << "* " << greeting << " *" << endl;
+	cout << second << endl;
+	cout << first << endl;
+
+	const string hello = "hello";
+	const string message = hello + ", world" + "!";
+
 	return 0;
 }
